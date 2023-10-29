f9138f5c9352dd6f4492fed07c69eb3f0a197588
Jason Chanliongco Maliquid <143939967+OMGItsJason@users.noreply.github.com>
2023-10-29 21:10:49 +0800

    Add files via upload

---
+++ Organization Profile/OrganizationProfile/OrganizationProfile.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Organization_Profile
+{
+    public partial class OrganizationProfile
+    {
+        StudentInformationClass StudentInformationClass;
+        long _StudentNo;
+        long _ContactNo;
+        string _FullName;
+        int _Age;
+        public OrganizationProfile()
+        {
+            InitializeComponent();
+            StudentInformationClass = new StudentInformationClass();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] ListOfProgram = new string[]
+            {
+                "BS Information Technology",
+                "BS Computer Science",
+                "BS Information System",
+                "BS in Accountancy",
+                "BS in Hospitality Management",
+                "BS in Tourism Management"
+            };
+            for (int i = 0; i < 6; i++)
+            {
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+            }
+            
+            string[] ListOfGender = new string[]
+            {
+                "Male",
+                "Female",
+                "Non-Binary"
+            };
+            for (int g = 0; g < 3; g++)
+            {
+                cbGender.Items.Add(ListOfGender[g].ToString());
+            }
+        }
+        
+        public class InvalidStudentNumberException : Exception
+        {
+            public InvalidStudentNumberException() : base("Invalid student number format.")
+            {
+            }
+        }
+
+        public class InvalidContactNumberException : Exception
+        {
+            public InvalidContactNumberException() : base("Invalid contact number format.")
+            {
+            }
+        }
+
+        public class InvalidAgeFormatException : Exception
+        {
+            public InvalidAgeFormatException() : base("Invalid age format.")
+            {
+            }
+        }
+
+        public class InvalidNameFormatException : Exception
+        {
+            public InvalidNameFormatException() : base("Invalid name format.")
+            {
+            }
+        }
+
+        private long StudentNumber(string studNum)
+        {
+            try
+            {
+                _StudentNo = long.Parse(studNum);
+                return _StudentNo;
+            }
+            catch (FormatException)
+            {
+                throw new InvalidStudentNumberException();
+            }
+        }
+
+        private long ContactNo(string Contact)
+        {
+            try
+            {
+                if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
+                {
+                    _ContactNo = long.Parse(Contact);
+                }
+                return _ContactNo;
+            }
+            catch (FormatException)
+            {
+                throw new InvalidContactNumberException();
+            }
+        }
+
+        private string FullName(string LastName, string FirstName, string MiddleInitial)
+        {
+            if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") || Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") || Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
+            {
+                _FullName = LastName + ", " + FirstName + ", " + MiddleInitial;
+            }
+
+            return _FullName;
+        }
+
+        private int Age(string age)
+        {
+            try
+            {
+                if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+                {
+                    _Age = int.Parse(age);
+                    return _Age;
+                }
+                throw new InvalidAgeFormatException();
+            }
+            catch (FormatException)
+            {
+                throw new InvalidAgeFormatException();
+            }
+        }
+        
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                
+                StudentInformationClass.SetFullName = FullName(txtLastName.Text, txtFirstName.Text, txtMiddleInitial.Text);
+                StudentInformationClass.SetStudentNo = StudentNumber(txtStudentNo.Text);
+                StudentInformationClass.SetProgram = cbPrograms.Text;
+                StudentInformationClass.SetGender = cbGender.Text;
+                StudentInformationClass.SetContactNo = ContactNo(TxtContactNo.Text);
+                StudentInformationClass.SetAge = Age(txtAge.Text);
+                StudentInformationClass.SetBirthday = datePickerBirthday.SelectedDate?.ToString("yyyy-MM-dd");
+
+                frmConfirmation frm = new frmConfirmation(StudentInformationClass);
+                frm.ShowDialog();
+            }
+            catch (InvalidStudentNumberException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            catch (InvalidContactNumberException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            catch (InvalidAgeFormatException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            catch (InvalidNameFormatException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void TxtStudentNo_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+    }
+
+    public class StudentInformationClass
+    {
+        public long SetStudentNo = 0;
+        public long SetContactNo = 0;
+        public string SetProgram = " ";
+        public string SetGender = " ";
+        public string SetBirthday = " ";
+        public string SetFullName = " ";
+        public int SetAge = 0;
+    }
+
+}
---
+++ Organization Profile/OrganizationProfile/frmConfirmation.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Organization_Profile
+{
+
+    public partial class frmConfirmation : Window
+    {
+        private string _FullName;
+        private int _Age;
+        private long _ContactNo;
+        private long _StudentNo;
+        private string _Birthday;
+        private string _Program;
+        private string _Gender;
+        private StudentInformationClass studentInfo;
+        
+        public frmConfirmation(StudentInformationClass studentInfo)
+        {
+            InitializeComponent();
+            this.studentInfo = studentInfo;
+        }
+
+        private void BtnSubmit_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitted Na Po","Thank You");
+        }
+
+        private void FrmConfirmation_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _FullName = studentInfo.SetFullName;
+            _Age = studentInfo.SetAge;
+            _ContactNo = studentInfo.SetContactNo;
+            _StudentNo = studentInfo.SetStudentNo;
+            _Birthday = studentInfo.SetBirthday;
+            _Program = studentInfo.SetProgram;
+            _Gender = studentInfo.SetGender;
+
+            lblName.Text = _FullName;
+            lblAge.Text = _Age.ToString();
+            lblContactNo.Text = _ContactNo.ToString();
+            lblStudentNo.Text = _StudentNo.ToString();
+            lblBirthday.Content = _Birthday;
+            lblProgram.Content = _Program;
+            lblGender.Content = _Gender;
+        }
+    }
+}
